423eef40d8309f96c70dc125730a6f29804ffed1
Antti Pollanen <antti.pollanen89@gmail.com>
2026-01-26 19:58:08 +0200

    Replace ready

--- ex04/incl/replace.hpp
+++ ex04/incl/replace.hpp
@@ -3,3 +3,16 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+
+class Replace
+{
+	public:
+		Replace(char *filename, char *str1, char *str2);
+		~Replace(void) = default;
+
+		std::string		_filename;
+		std::string		_str1;
+		std::string		_str2;
+		std::ifstream	_infile;
+		std::ofstream	_outfile;
+};
